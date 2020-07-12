7e008f3052c83c3b787c3dae07eb3415948ba737
caspar <279397942@qq.com>
2020-07-12 14:54:57 +0800

    init

--- src/ace/devops/module/util/HttpUtils.groovy
+++ src/ace/devops/module/util/HttpUtils.groovy
@@ -27,28 +27,25 @@ class HttpUtils {
         )
         String body = "";
         HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(realUrl).openConnection()
-        try {
-            httpURLConnection.setConnectTimeout(DEFAULT_CONNECTION_TIMEOUT);
-            httpURLConnection.setReadTimeout(DEFAULT_READ_TIMEOUT)
-            httpURLConnection.setRequestMethod(REQUEST_METHOD_GET);
 
-            httpURLConnection.connect();
-            //返回打开连接读取的输入流，输入流转化为StringBuffer类型，这一套流程要记住，常用
-            try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()))) {
-                StringBuffer stringBuffer = new StringBuffer();
-                String line = "";
-                while ((line = bufferedReader.readLine()) != null) {
-                    stringBuffer.append(line);
-                }
-                body = new String(stringBuffer.toString().getBytes(), "UTF-8");
-                bufferedReader.close();
-            }
-            httpURLConnection.disconnect();
-        } catch (Exception ex) {
-            if (httpURLConnection != null) {
-                httpURLConnection.disconnect();
-            }
+        httpURLConnection.setConnectTimeout(DEFAULT_CONNECTION_TIMEOUT);
+        httpURLConnection.setReadTimeout(DEFAULT_READ_TIMEOUT)
+        httpURLConnection.setRequestMethod(REQUEST_METHOD_GET);
+
+        httpURLConnection.connect();
+        //返回打开连接读取的输入流，输入流转化为StringBuffer类型，这一套流程要记住，常用
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+
+        StringBuffer stringBuffer = new StringBuffer();
+        String line = "";
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuffer.append(line);
         }
+        body = new String(stringBuffer.toString().getBytes(), "UTF-8");
+        bufferedReader.close();
+
+        httpURLConnection.disconnect();
+
         return body;
     }
 }
